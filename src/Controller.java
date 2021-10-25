911f38b111a37393e0a65279d8ca23e940c477ec
Cerberus <cerberusprogrammer@gmail.com>
2021-10-25 15:41:25 -0700

    Nuevos metodos de ordenamientos agregados en Ordenamientos.java

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,12 @@
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.ResourceBundle;
 import java.util.concurrent.TimeUnit;
 
@@ -14,41 +15,87 @@ public class Controller implements Initializable {
     @FXML
     private BarChart<String, Number> barChart;
 
-    int[] array1;
-    int[] array2;
-    int[] array3;
-    int[] array4;
-    int[] array5;
-    int[] array6;
-    int[] array7;
-    int[] array8;
-    int[] array9;
-    int[] array10;
 
-    void createArrays() {
-        int size = 100;
+    ArrayList<int[]> arrays = new ArrayList<>();
 
+    void createArrays(int amountArrays, int sizeElements) {
+        for (int i = 0; i < amountArrays; i++) {
+            int[] array = randomNumbers(sizeElements);
+            arrays.add(array);
+        }
     }
 
-    void randomNumbers(int size) {
+    int[] randomNumbers(int size) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
 
+        for (int i = 0; i < size; i++)
+            arrayList.add(i);
+
+        Collections.shuffle(arrayList);
+        int[] ints = new int[size];
+
+        for (int i = 0; i < size; i++)
+            ints[i] = arrayList.get(i);
+
+        return ints;
+    }
+
+    void nextOrden(int orden, int[] A) {
+        switch (orden) {
+            case 0:
+                Ordenamientos.insercion(A);
+                break;
+            case 1:
+                Ordenamientos.seleccion(A);
+                break;
+            case 2:
+                Ordenamientos.shell(A);
+                break;
+            case 3:
+                Ordenamientos.quicksort(A, 0, 0);
+                break;
+            case 4:
+                Ordenamientos.bucketSort(A, 0);
+                break;
+            case 5:
+                // Ordenamientos.radixSort(A);
+                break;
+            case 6:
+                Ordenamientos.sort(A);
+                break;
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        int[] array = {1,2, 3,4};
+        createArrays(6, 100);
+        ArrayList<XYChart.Series> seriesArrayList = new ArrayList<>();
+        int eleccionOrden = 0;
+        String[] strings = {
+                "Insercion",
+                "Seleccion",
+                "Shell",
+                "QuickSort",
+                "BucketSort",
+                "RadixSort",
+                "Arrays.sort()"
+        };
 
-        long startTime = System.nanoTime();
-        Ordenamientos.insercion(array);
-        long endTime = System.nanoTime();
+        for (int[] ints: arrays) {
+            long startTime = System.nanoTime();
+            nextOrden(eleccionOrden, ints);
+            long endTime = System.nanoTime();
+            long totalTime = endTime - startTime;
+            // TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS)
+            eleccionOrden++;
 
-        XYChart.Series seriesInsercion = new XYChart.Series();
-        seriesInsercion.setName("Insercion");
-        seriesInsercion.getData().add(new XYChart.Data("Insercion",
-                TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS)));
+            XYChart.Series seriesInsercion = new XYChart.Series();
+            seriesInsercion.setName(strings[eleccionOrden]);
+            seriesInsercion.getData().add(new XYChart.Data(strings[eleccionOrden], totalTime));
+            seriesArrayList.add(seriesInsercion);
+        }
 
-        barChart.getXAxis().setLabel("Metodo");
-        barChart.getYAxis().setLabel("Tiempo (ms)");
-        barChart.getData().addAll(seriesInsercion);
+        for (XYChart.Series series: seriesArrayList)
+            barChart.getData().add(series);
     }
 }
--- src/Ordenamientos.java
+++ src/Ordenamientos.java
@@ -1,23 +1,141 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Vector;
+
 public class Ordenamientos {
 
-    public static int[] insercion(int[] A){
+    public static void insercion(int[] A) {
         int p, j;
         int aux;
 
-        for (p = 1; p < A.length; p++){
+        for (p = 1; p < A.length; p++) {
             aux = A[p];
             j = p - 1;
 
-            while ((j >= 0) && (aux < A[j])){
+            while ((j >= 0) && (aux < A[j])) {
                 A[j + 1] = A[j];
                 j--;
             }
 
             A[j + 1] = aux;
         }
-
-        return A;
     }
 
+    public static void seleccion(int[] A) {
+        int i, j, menor, pos, tmp;
+        for (i = 0; i < A.length - 1; i++) {
+            menor = A[i];
+            pos = i;
+            for (j = i + 1; j < A.length; j++) {
+                if (A[j] < menor) {
+                    menor = A[j];
+                    pos = j;
+                }
+            }
+            if (pos != i) {
+                tmp = A[i];
+                A[i] = A[pos];
+                A[pos] = tmp;
+            }
+        }
+    }
 
+    public static void shell(int[] A) {
+        int salto, aux, i;
+        boolean cambios;
+
+        for (salto = A.length / 2; salto != 0; salto /= 2) {
+            cambios = true;
+            while (cambios) {
+                cambios = false;
+                for (i = salto; i < A.length; i++) {
+                    if (A[i - salto] > A[i]) {
+                        aux = A[i];
+                        A[i] = A[i - salto];
+                        A[i - salto] = aux;
+                        cambios = true;
+                    }
+                }
+            }
+        }
+    }
+
+    public static void quicksort(int[] A, int izq, int der) {
+        int pivote = A[izq];
+        int i = izq;
+        int j = der;
+        int aux;
+
+        while (i < j) {
+            while (A[i] <= pivote && i < j) i++;
+            while (A[j] > pivote) j--;
+            if (i < j) {
+                aux = A[i];
+                A[i] = A[j];
+                A[j] = aux;
+            }
+        }
+
+        A[izq] = A[j];
+        A[j] = pivote;
+
+        if (izq < j - 1)
+            quicksort(A, izq, j - 1);
+        if (j + 1 < der)
+            quicksort(A, j + 1, der);
+    }
+
+    static void bucketSort(int arr[], int n) {
+        if (n <= 0)
+            return;
+
+        @SuppressWarnings("unchecked")
+        Vector<Integer>[] buckets = new Vector[n];
+
+        for (int i = 0; i < n; i++)
+            buckets[i] = new Vector<Integer>();
+
+        for (int i = 0; i < n; i++) {
+            float idx = arr[i] * n;
+            buckets[(int) idx].add(arr[i]);
+        }
+
+        for (int i = 0; i < n; i++)
+            Collections.sort(buckets[i]);
+
+        int index = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < buckets[i].size(); j++)
+                arr[index++] = buckets[i].get(j);
+    }
+
+    static void radixSort(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (max < arr[i])
+                max = arr[i];
+        }
+
+        for (int s = 1; max / s > 0; s *= 10) {
+            int[] countingArray = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            for (int i = 0; i < arr.length; i++)
+                countingArray[(arr[i] / s) % 10]++;
+
+            for (int i = 1; i < 10; i++)
+                countingArray[i] += countingArray[i - 1];
+
+            int[] outputArray = {0, 0, 0, 0, 0, 0, 0, 0};
+            for (int i = arr.length - 1; i >= 0; i--)
+                outputArray[--countingArray[(arr[i] / s) % 10]] = arr[i];
+
+            for (int i = 0; i < arr.length; i++)
+                arr[i] = outputArray[i];
+        }
+    }
+
+    static void sort(int[] A) {
+        Arrays.sort(A);
+    }
 }
